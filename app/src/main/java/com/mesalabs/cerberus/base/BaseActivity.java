e5a90e22e045201ce17d503cac422c84969e160f
BlackMesa123 <brother12@hotmail.it>
2019-06-30 17:38:54 +0200

    app/ui: add OneUI-like AppBar
    Signed-off-by: BlackMesa123 <brother12@hotmail.it>

--- app/src/main/java/com/mesalabs/cerberus/base/BaseActivity.java
+++ app/src/main/java/com/mesalabs/cerberus/base/BaseActivity.java
@@ -1,9 +1,21 @@
 package com.mesalabs.cerberus.base;
 
 import android.content.Context;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.view.ViewStub;
 
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.mesalabs.cerberus.R;
+import com.mesalabs.cerberus.utils.SharedPreferencesUtils;
+import com.mesalabs.cerberus.utils.Utils;
 
 /*
  * Cerberus Core App
@@ -22,12 +34,92 @@ import android.support.v7.app.AppCompatActivity;
 
 public class BaseActivity extends AppCompatActivity {
     protected Context mContext;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         mContext = this;
+
+    }
+
+    // AppBar related methods
+
+    /**
+     * createAppBar (expanded, no homeAsUp, title)
+     */
+    protected void createAppBar(int mTitleTextId) {
+        createAppBar(true, false, 0, mTitleTextId);
+    }
+
+    /**
+     * createAppBar (expanded, no homeAsUp, title)
+     */
+    protected void createAppBar(boolean mIsExpanded, int mTitleTextId) {
+        createAppBar(mIsExpanded, false, 0, mTitleTextId);
+    }
+
+    /**
+     * createAppBar (expanded, homeAsUp, title)
+     */
+    protected void createAppBar(boolean mIsExpanded, boolean mShowHomeAsUp, int mTitleTextId) {
+        createAppBar(mIsExpanded, mShowHomeAsUp, 0, mTitleTextId);
+    }
+
+    /**
+     * createAppBar (expanded, homeAsUp with custom icon, title)
+     */
+    protected void createAppBar(boolean mIsExpanded, boolean mShowHomeAsUp, int mCustomHAUicId, int mTitleTextId) {
+        Toolbar toolbar = findViewById(R.id.mesa_toolbar_appbarlayout);
+        setSupportActionBar(toolbar);
+        if (toolbar != null) {
+            getSupportActionBar().setDisplayShowTitleEnabled(true);
+            getSupportActionBar().setTitle("");
+
+            if (mShowHomeAsUp) {
+                getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+                if (mCustomHAUicId != 0) {
+                    getSupportActionBar().setDisplayShowHomeEnabled(false);
+                    getSupportActionBar().setHomeAsUpIndicator(abGenCustomHomeAsUpIcon(mCustomHAUicId));
+                } else
+                    getSupportActionBar().setDisplayShowHomeEnabled(true);
+            } else
+                getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+
+            ViewStub stubAbHeader = findViewById(R.id.mesa_viewstub_appbarlayout);
+            stubAbHeader.setLayoutResource(R.layout.mesa_header_appbarlayout);
+
+            stubAbHeader.inflate();
+
+            final AppBarLayout appBarLayout = findViewById(R.id.mesa_appbar_appbarlayout);
+            final LinearLayout abExpaTitleLayout = findViewById(R.id.mesa_abheaderlayout_appbarlayout);
+            TextView abExpaTitle = findViewById(R.id.mesa_abexpatitle_appbarlayout);
+            final TextView abCollTitle = findViewById(R.id.mesa_toolbartitle_appbarlayout);
+
+            appBarLayout.setExpanded(mIsExpanded);
+
+            if (mTitleTextId != 0) {
+                abExpaTitle.setText(getString(mTitleTextId));
+                abCollTitle.setText(getString(mTitleTextId));
+            }
+
+            appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+                @Override
+                public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                    float range = (((float) Math.abs(verticalOffset)) / ((float) appBarLayout.getTotalScrollRange())) * 2.0f;
+                    abExpaTitleLayout.setAlpha(1.5f - range);
+                    abCollTitle.setAlpha(range - 1.0f);
+                }
+            });
+
+        }
+    }
+
+    private Drawable abGenCustomHomeAsUpIcon(int mCustomHAUicId) {
+        final Drawable tbCustomIcon = getResources().getDrawable(mCustomHAUicId, mContext.getTheme());
+        int c = getResources().getColor(Utils.isNightMode(this) ? R.color.sesl_primary_text_color_dark : R.color.sesl_primary_text_color_light, mContext.getTheme());
+        tbCustomIcon.setColorFilter(c, PorterDuff.Mode.SRC_ATOP);
+        return tbCustomIcon;
     }
 
 }
