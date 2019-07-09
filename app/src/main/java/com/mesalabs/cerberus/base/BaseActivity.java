b680457c48be32c6c20c2f2f44d7bd6e04fc0ba3
BlackMesa123 <brother12@hotmail.it>
2019-07-09 23:15:22 +0200

    app/ui: refactor OneUI AppBar
    -Disable AppBar in MultiWindow/Landscape mode
    -1:1 size/parallax effect from Sesl API
    -Replaced homeAsUp with a modular ImageButton View
    
    More to be added soon probably...
    
    Signed-off-by: BlackMesa123 <brother12@hotmail.it>

--- app/src/main/java/com/mesalabs/cerberus/base/BaseActivity.java
+++ app/src/main/java/com/mesalabs/cerberus/base/BaseActivity.java
@@ -1,21 +1,28 @@
 package com.mesalabs.cerberus.base;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.res.Configuration;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.support.design.widget.AppBarLayout;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.ViewStub;
-
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatImageButton;
+import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.TooltipCompat;
+
 import com.mesalabs.cerberus.R;
-import com.mesalabs.cerberus.utils.SharedPreferencesUtils;
 import com.mesalabs.cerberus.utils.Utils;
+import com.mesalabs.cerberus.utils.ViewUtils;
 
 /*
  * Cerberus Core App
@@ -34,6 +41,11 @@ import com.mesalabs.cerberus.utils.Utils;
 
 public class BaseActivity extends AppCompatActivity {
     protected Context mContext;
+
+    private AppBarLayout abAppBarLayout;
+    private AppCompatImageButton abHomeButton;
+    private float mDefaultHeightDp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,84 +54,164 @@ public class BaseActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        resetAppBarHeight();
+    }
+
     // AppBar related methods
 
     /**
      * createAppBar (expanded, no homeAsUp, title)
      */
-    protected void createAppBar(int mTitleTextId) {
-        createAppBar(true, false, 0, mTitleTextId);
+    protected void createAppBar(int titleTextId) {
+        createAppBar(true, false, titleTextId);
     }
 
     /**
      * createAppBar (expanded, no homeAsUp, title)
      */
-    protected void createAppBar(boolean mIsExpanded, int mTitleTextId) {
-        createAppBar(mIsExpanded, false, 0, mTitleTextId);
+    protected void createAppBar(boolean isExpanded, int titleTextId) {
+        createAppBar(isExpanded, false,  titleTextId);
     }
 
     /**
      * createAppBar (expanded, homeAsUp, title)
      */
-    protected void createAppBar(boolean mIsExpanded, boolean mShowHomeAsUp, int mTitleTextId) {
-        createAppBar(mIsExpanded, mShowHomeAsUp, 0, mTitleTextId);
-    }
+    protected void createAppBar(boolean isExpanded, boolean showHomeAsUp, int titleTextId) {
+        Toolbar abToolbar = findViewById(R.id.mesa_toolbar_appbarlayout);
+        setSupportActionBar(abToolbar);
+        if (abToolbar != null) {
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(false);
 
-    /**
-     * createAppBar (expanded, homeAsUp with custom icon, title)
-     */
-    protected void createAppBar(boolean mIsExpanded, boolean mShowHomeAsUp, int mCustomHAUicId, int mTitleTextId) {
-        Toolbar toolbar = findViewById(R.id.mesa_toolbar_appbarlayout);
-        setSupportActionBar(toolbar);
-        if (toolbar != null) {
-            getSupportActionBar().setDisplayShowTitleEnabled(true);
-            getSupportActionBar().setTitle("");
+            abAppBarLayout = findViewById(R.id.mesa_appbar_appbarlayout);
+            final CollapsingToolbarLayout abCollapsingToolbarLayout = findViewById(R.id.mesa_colltoolbar_appbarlayout);
 
-            if (mShowHomeAsUp) {
-                getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            // Inflate header instead of adding it inside AppBar xml for future usages...
+            ViewStub abStubAppBarHeader = findViewById(R.id.mesa_viewstub_appbarlayout);
+            abStubAppBarHeader.setLayoutResource(R.layout.mesa_header_appbarlayout);
 
-                if (mCustomHAUicId != 0) {
-                    getSupportActionBar().setDisplayShowHomeEnabled(false);
-                    getSupportActionBar().setHomeAsUpIndicator(abGenCustomHomeAsUpIcon(mCustomHAUicId));
-                } else
-                    getSupportActionBar().setDisplayShowHomeEnabled(true);
-            } else
-                getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+            abStubAppBarHeader.inflate();
 
-            ViewStub stubAbHeader = findViewById(R.id.mesa_viewstub_appbarlayout);
-            stubAbHeader.setLayoutResource(R.layout.mesa_header_appbarlayout);
+            final LinearLayout abExpandedTitleLayout = findViewById(R.id.mesa_abheaderlayout_appbarlayout);
+            TextView abExpandedTitle = findViewById(R.id.mesa_abexpatitle_appbarlayout);
+            abHomeButton = findViewById(R.id.mesa_toolbarhome_appbarlayout);
+            final TextView abCollapsedTitle = findViewById(R.id.mesa_toolbartitle_appbarlayout);
 
-            stubAbHeader.inflate();
+            resetAppBarHeight();
+            abAppBarLayout.setExpanded(isExpanded);
 
-            final AppBarLayout appBarLayout = findViewById(R.id.mesa_appbar_appbarlayout);
-            final LinearLayout abExpaTitleLayout = findViewById(R.id.mesa_abheaderlayout_appbarlayout);
-            TextView abExpaTitle = findViewById(R.id.mesa_abexpatitle_appbarlayout);
-            final TextView abCollTitle = findViewById(R.id.mesa_toolbartitle_appbarlayout);
+            if (showHomeAsUp) {
+                abHomeButton.setVisibility(View.VISIBLE);
 
-            appBarLayout.setExpanded(mIsExpanded);
+                abCollapsedTitle.setPadding(0, 0, 0, 0);
 
-            if (mTitleTextId != 0) {
-                abExpaTitle.setText(getString(mTitleTextId));
-                abCollTitle.setText(getString(mTitleTextId));
+                TooltipCompat.setTooltipText(abHomeButton, getString(R.string.sesl_action_bar_up_description));
+
+                setToolbarHomeIconOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        onBackPressed();
+                    }
+                });
             }
 
-            appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            if (titleTextId != 0) {
+                abExpandedTitle.setText(getString(titleTextId));
+                abCollapsedTitle.setText(getString(titleTextId));
+            }
+
+            abAppBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+                @SuppressLint("Range")
                 @Override
                 public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
-                    float range = (((float) Math.abs(verticalOffset)) / ((float) appBarLayout.getTotalScrollRange())) * 2.0f;
-                    abExpaTitleLayout.setAlpha(1.5f - range);
-                    abCollTitle.setAlpha(range - 1.0f);
+                    int layoutPosition = Math.abs(appBarLayout.getTop());
+                    float alphaRange = ((float) abCollapsingToolbarLayout.getHeight()) * 0.17999999f;
+                    float toolbarTitleAlphaStart = ((float) abCollapsingToolbarLayout.getHeight()) * 0.35f;
+
+                    abExpandedTitleLayout.setTranslationY((float) ((-verticalOffset) / 3));
+
+                    float expaTitleAlpha = 255.0f - ((100.0f / alphaRange) * (((float) layoutPosition) - 0.0f));
+                    if (expaTitleAlpha < 0.0f) {
+                        expaTitleAlpha = 0.0f;
+                    } else if (expaTitleAlpha > 255.0f) {
+                        expaTitleAlpha = 255.0f;
+                    }
+                    expaTitleAlpha /= 255.0f;
+
+                    abExpandedTitleLayout.setAlpha(expaTitleAlpha);
+
+                    if (appBarLayout.getHeight() <= ((int) mDefaultHeightDp)) {
+                        abExpandedTitleLayout.setAlpha(0.0f);
+                        abCollapsedTitle.setAlpha(1.0f);
+                    } else {
+                        double collapsedTitleAlpha = (double) ((150.0f / alphaRange) * (((float) layoutPosition) - toolbarTitleAlphaStart));
+
+                        if (collapsedTitleAlpha >= 0.0d && collapsedTitleAlpha <= 255.0d) {
+                            collapsedTitleAlpha /= 255.0d;
+                            abCollapsedTitle.setAlpha((float) collapsedTitleAlpha);
+                        }
+                        else if (collapsedTitleAlpha < 0.0d)
+                            abCollapsedTitle.setAlpha(0.0f);
+                        else
+                            abCollapsedTitle.setAlpha(1.0f);
+                    }
+
                 }
             });
 
         }
     }
 
-    private Drawable abGenCustomHomeAsUpIcon(int mCustomHAUicId) {
-        final Drawable tbCustomIcon = getResources().getDrawable(mCustomHAUicId, mContext.getTheme());
-        int c = getResources().getColor(Utils.isNightMode(this) ? R.color.sesl_primary_text_color_dark : R.color.sesl_primary_text_color_light, mContext.getTheme());
-        tbCustomIcon.setColorFilter(c, PorterDuff.Mode.SRC_ATOP);
-        return tbCustomIcon;
+    private void customizeToolbarHomeIcon(int homeButtonIcId, int homeButtonDescId) {
+        if (homeButtonIcId <= 0 || homeButtonDescId <= 0) return;
+
+        final Drawable abHomeButtonIcon = getResources().getDrawable(homeButtonIcId, mContext.getTheme());
+        int color = getResources().getColor(Utils.isNightMode(this) ? R.color.sesl_primary_text_color_dark : R.color.sesl_primary_text_color_light, mContext.getTheme());
+        abHomeButtonIcon.setColorFilter(color, PorterDuff.Mode.SRC_ATOP);
+
+        abHomeButton.setImageDrawable(abHomeButtonIcon);
+
+        TooltipCompat.setTooltipText(abHomeButton, getString(homeButtonDescId));
+    }
+
+    protected void resetAppBarHeight() {
+        if (abAppBarLayout == null) return;
+
+        ViewGroup.LayoutParams params = abAppBarLayout.getLayoutParams();
+        int windowHeight = ViewUtils.getWindowHeight(this);
+
+        if (ViewUtils.isLandscape() || Utils.isInMultiWindowMode(this)) {
+            abAppBarLayout.setExpanded(false, false);
+            abAppBarLayout.setActivated(false);
+
+            mDefaultHeightDp = getResources().getDimension(R.dimen.sesl_action_bar_default_height_padding);
+
+            params.height = (int) mDefaultHeightDp;
+        } else {
+            abAppBarLayout.setExpanded(true, false);
+            abAppBarLayout.setActivated(true);
+
+            TypedValue outValue = new TypedValue();
+            getResources().getValue(R.dimen.sesl_abl_height_proportion, outValue, true);
+
+            mDefaultHeightDp = getResources().getDimension(R.dimen.sesl_action_bar_default_height);
+
+            params.height = (int) ((float) windowHeight * outValue.getFloat());
+        }
+
+        abAppBarLayout.setLayoutParams(params);
+
+    }
+
+    protected void setToolbarHomeIconOnClickListener(View.OnClickListener ocl) {
+        if (abHomeButton == null) return;
+
+        if (ocl != null)
+            abHomeButton.setOnClickListener(ocl);
     }
 
 }
