3184593185824b3cbd184338bd546bf3aef9c291
BlackMesa123 <brother12@hotmail.it>
2019-07-09 17:34:25 +0200

    app/java: add LogUtils class
    Signed-off-by: BlackMesa123 <brother12@hotmail.it>

---
+++ app/src/main/java/com/mesalabs/cerberus/utils/LogUtils.java
@@ -0,0 +1,52 @@
+package com.mesalabs.cerberus.utils;
+
+import android.util.Log;
+
+import com.mesalabs.cerberus.CerberusApp;
+
+/*
+ * Cerberus Core App
+ *
+ * Coded by BlackMesa @2019
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * ULTRA-MEGA-PRIVATE SOURCE CODE. SHARING TO DEVKINGS TEAM
+ * EXTERNALS IS PROHIBITED AND WILL BE PUNISHED WITH ANAL ABUSE.
+ */
+
+public class LogUtils {
+    // Verbose
+    public static void v(String tag, String msg) {
+        if (CerberusApp.isDebugBuild())
+            Log.v("CerberusCore: " + tag, msg);
+    }
+
+    // Debug
+    public static void d(String tag, String msg) {
+        if (CerberusApp.isDebugBuild())
+            Log.d("CerberusCore: " + tag, msg);
+    }
+
+    // Info
+    public static void i(String tag, String msg) {
+        if (CerberusApp.isDebugBuild())
+            Log.i("CerberusCore: " + tag, msg);
+    }
+
+    // Warn
+    public static void w(String tag, String msg) {
+        if (CerberusApp.isDebugBuild())
+            Log.w("CerberusCore: " + tag, msg);
+    }
+
+    // Error
+    public static void e(String tag, String msg) {
+        if (CerberusApp.isDebugBuild())
+            Log.e("CerberusCore: " + tag, msg);
+    }
+}
