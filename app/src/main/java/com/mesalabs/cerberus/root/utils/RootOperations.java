7b6fdb1b901806a86cfa7308c889332dd316de87
BlackMesa123 <brother12@hotmail.it>
2020-02-21 15:39:07 +0100

    app: add root utils
    Signed-off-by: BlackMesa123 <brother12@hotmail.it>

---
+++ app/src/main/java/com/mesalabs/cerberus/root/utils/RootOperations.java
@@ -0,0 +1,70 @@
+package com.mesalabs.cerberus.root.utils;
+
+import android.content.Context;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import com.mesalabs.cerberus.root.SU;
+import com.mesalabs.cerberus.utils.LogUtils;
+
+/*
+ * Cerberus Core App
+ *
+ * Coded by BlackMesa @2020
+ * Original code by Willi Ye & AndreiLux
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * ULTRA-MEGA-PRIVATE SOURCE CODE. SHARING TO DEVKINGS TEAM
+ * EXTERNALS IS PROHIBITED AND WILL BE PUNISHED WITH ANAL ABUSE.
+ */
+
+public class RootOperations {
+    private static String TAG = "RootOperations";
+
+    public static boolean existFile(String file) {
+        return existFile(file, true);
+    }
+
+    public static boolean existFile(String file, boolean root) {
+        return existFile(file, root ? RootUtils.getSU() : null);
+    }
+
+    public static boolean existFile(String file, SU su) {
+        return su == null ? new File(file).exists() : new RootFile(file, su).exists();
+    }
+
+    public static String readAssetFile(Context context, String file) {
+        InputStream input = null;
+        BufferedReader buf = null;
+        try {
+            StringBuilder s = new StringBuilder();
+            input = context.getAssets().open(file);
+            buf = new BufferedReader(new InputStreamReader(input));
+
+            String str;
+            while ((str = buf.readLine()) != null) {
+                s.append(str).append("\n");
+            }
+            return s.toString().trim();
+        } catch (IOException e) {
+            LogUtils.e(TAG, "Unable to read " + file);
+        } finally {
+            try {
+                if (input != null) input.close();
+                if (buf != null) buf.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+}
