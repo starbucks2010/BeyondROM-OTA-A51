34d0a7ef28263e20a3eb1b298b5e66e2baadc6b7
Pachadomenic <pachadomenic2010@gmail.com>
2023-11-18 13:29:28 +0200

    Use m31 manifest

--- app/src/main/java/com/mesalabs/ten/update/ota/utils/Constants.java
+++ app/src/main/java/com/mesalabs/ten/update/ota/utils/Constants.java
@@ -1,4 +1,4 @@
-package com.mesalabs.ten.update.ota.utils;
+kipackage com.mesalabs.ten.update.ota.utils;
 
 /*
  * 십 Update
@@ -19,7 +19,7 @@ public class Constants {
 
     // Manifest
     //public static final String OTA_MANIFEST_URL = "https://gitlab.com/BlackMesa123/on-otamanifest/raw/master/" + PropUtils.get(PROP_ROM_BUILD, "0") + "/rom-manifest.xml";
-    public static final String OTA_MANIFEST_URL = "https://raw.githubusercontent.com/ShaDisNX255/NcX_OnUpdate/main/a52q/manifest.xml";
+    public static final String OTA_MANIFEST_URL = "https://raw.githubusercontent.com/pachdomenic/TukiRom-OTA/manifest/m31/manifest.xml";
 
     // Broadcast intents
     public static final String INTENT_MANIFEST_LOADED = "com.mesalabs.ten.ota.MANIFEST_LOADED";
